e73bfcb417870b5572a40d0d59aee8fe70754755
Piotr Zywczewski <piotr.zywczewski@gmail.com>
2025-04-22 12:16:18 +0200

    Detekt
    Setup Detekt configuration for whole project. It will be used for static code analysis to keep consistency in written code.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,20 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.detekt)
+}
+
+tasks.register("detektAllModules") {
+    subprojects.forEach { subproject ->
+
+        require(subproject.pluginManager.hasPlugin(libs.plugins.detekt.get().pluginId)) {
+            "Module '${subproject.name}' doesn't use Detekt plugin. You need to add 'alias(libs.plugins.detekt)' to the module."
+        }
+
+        subproject.detekt {
+            parallel = true
+            config.setFrom(files("$rootDir/code_quality/detekt/detekt.yml"))
+        }
+        finalizedBy(":${subproject.name}:detekt")
+    }
 }
